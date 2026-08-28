71eac1e8a8fe8563e10c9096927ecb20512dcb3b
Luis Abad <pablo.anchiraicoabad@gmail.com>
2026-08-27 22:12:28 -0500

    Agrega data class Producto y funciones de subtotal, IGV y total

---
+++ Semana02/app/README.md
@@ -0,0 +1,8 @@
+# Lab02 - Carrito de Compras en Kotlin
+¿Por qué nombre y precio son val pero cantidad es var?
+
+Porque val es una variable solo de lectura y var es una variable la cual puede cambiar y como es una cantidad puede variar.
+
+¿Qué pasaría si intentas cambiar el precio después de crear el producto?
+
+Saldria error por1ue val no lo permitiria.
--- Semana02/app/src/main/java/com/abad/lab02carritokotlin/Carrito.kt
+++ Semana02/app/src/main/java/com/abad/lab02carritokotlin/Carrito.kt
@@ -1,7 +1,54 @@
 package com.abad.lab02carritokotlin
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+
+fun calcularIGV(subtotal: Double): Double {
+    val igv = subtotal * 0.18
+    return igv
+}
+
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    val total = subtotal + igv
+    return total
+}
+
 fun main() {
     println("=========================================")
     println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
     println("=========================================")
+
+    val nombreCliente = "Luis Abad"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente")
+    println()
+
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 1))
+    carrito.add(Producto("Teclado Redragon", 120.0, 3))
+    carrito.add(Producto("Monitor msi", 650.0, 1))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIGV(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+    println()
+    println("Subtotal: $subtotal")
+    println("IGV: $igv")
+    println("Total: $total")
 }
