9adb00fc8b8a7eab19bcdc1c1a022603eac52c39
Luis Abad <pablo.anchiraicoabad@gmail.com>
2026-09-22 20:46:38 -0500

    Agrega AppNavigation con NavHost y rutas configuradas

--- Semana05/app/src/main/java/com/abad/lab05navegacion/navigation/AppNavigation.kt
+++ Semana05/app/src/main/java/com/abad/lab05navegacion/navigation/AppNavigation.kt
@@ -1,2 +1,33 @@
 package com.abad.lab05navegacion.navigation
 
+import android.R.attr.type
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(Screen.Home.route) {
+            // TODO: llamar a HomeScreen(navController)
+        }
+        composable(Screen.List.route) {
+            // TODO: llamar a ListScreen(navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            // TODO: llamar a DetailScreen(itemId, navController)
+        }
+        composable(Screen.Profile.route) {
+            // TODO: llamar a ProfileScreen(navController)
+        }
+    }
+}
